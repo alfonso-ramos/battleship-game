45ae8cbbbec7d92bc775822b56032df2d3c62678
Alfonso <alfonsoramos.1803@gmail.com>
2022-12-22 20:56:33 -0700

    [ADD] Vistas para el single player y el multiplayer hechas

---
+++ public/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const userGrid = document.querySelector('.grid-user')
+  const cpuGrid = document.querySelector('.grid-user')
+  const displayGrid = document.querySelector('.grid-display')
+
+  const ships = document.querySelector('.ship')
+})
